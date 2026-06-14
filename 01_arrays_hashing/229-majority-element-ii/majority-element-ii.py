2675e101c9a9ac6818ca3f730e52b1eedd421e92
Rajendra Prasad Joshi <rajendraprasadjoshi22@gmail.com>
2026-06-14 12:27:33 +0530

    Time: 11 ms (35.53%) | Memory: 22.1 MB (68.52%) - LeetSync

---
+++ 01_arrays_hashing/229-majority-element-ii/majority-element-ii.py
@@ -0,0 +1,44 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        # for the first majority element
+        count1=0
+        majority1=None
+        
+        # for the second majority element
+        count2=0
+        majority2=None
+
+        # step 1: Assumption
+        for i in nums:
+            if majority1==i:
+                count1+=1
+            elif majority2==i:
+                count2+=1
+            elif count1==0:
+                majority1=i
+                count1=1
+            elif count2==0:
+                majority2=i
+                count2=1
+            else:
+                count1-=1
+                count2-=1
+        
+        # step 2:verification
+        counter1=0
+        counter2=0
+
+        for i in nums:
+            if i==majority1:
+                counter1+=1
+            if i==majority2:
+                counter2+=1
+            
+        if counter1>len(nums)//3 and counter2>len(nums)//3:
+            return [majority1,majority2]
+        elif counter1>len(nums)//3:
+            return [majority1]
+        elif counter2>len(nums)//3:
+            return [majority2]
+        else:
+            return []
