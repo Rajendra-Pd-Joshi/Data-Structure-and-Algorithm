2addb33b14f66d6ae7ebdc9e35c5dfcde1d53ecf
Rajendra Prasad Joshi <rajendraprasadjoshi22@gmail.com>
2026-06-16 10:05:26 +0530

    Time: 32 ms (67.33%) | Memory: 24.8 MB (52.79%) - LeetSync

--- 01_arrays_hashing/816-design-hashset/design-hashset.py
+++ 01_arrays_hashing/816-design-hashset/design-hashset.py
@@ -1,17 +1,39 @@
+class ListNode:
+    def __init__(self,key:int):
+        self.key=key
+        self.next=None
 class MyHashSet:
 
     def __init__(self):
-        self.arr=[False]*1000001
+        self.set=[ListNode(0) for i in range(1000)]
 
     def add(self, key: int) -> None:
-        self.arr[key]=True
+        cur=self.set[key%1000]
+        
+        while cur.next:
+            if cur.next.key==key:
+                return
+            cur=cur.next
+        cur.next=ListNode(key)
 
     def remove(self, key: int) -> None:
-        self.arr[key]=False
+        cur=self.set[key%1000]
+
+        while cur.next:
+            if cur.next.key==key:
+                cur.next=cur.next.next
+                return
+            cur=cur.next
 
     def contains(self, key: int) -> bool:
-        return self.arr[key]
-
+        
+        cur=self.set[key%1000]
+        
+        while cur.next:
+            if cur.next.key==key:
+                return True
+            cur=cur.next
+        return False
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
