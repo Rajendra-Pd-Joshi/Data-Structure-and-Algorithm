269400093b1b12b844a1d0db0b0f7af571d05c87
Rajendra Prasad Joshi <rajendraprasadjoshi22@gmail.com>
2026-06-25 11:55:14 +0530

    Time: 9 ms (21.63%) | Memory: 19.3 MB (23.44%) - LeetSync

---
+++ 05_binary_search/69-sqrtx/sqrtx.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l,r = 1,x
+        while l <= r :
+            m = (l+r)//2
+            print(m)
+            if m*m > x:
+                r = m-1
+            elif m*m < x:
+                l = m+1
+            else:
+                return m
+        return l-1
