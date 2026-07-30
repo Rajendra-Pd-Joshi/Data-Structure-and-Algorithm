e16d2b7d5015834b2f1560804f2f82fcd95a6314
Rajendra Prasad Joshi <rajendraprasadjoshi22@gmail.com>
2026-07-30 13:56:28 +0530

    Time: 43 ms (69.92%) | Memory: 42.8 MB (73.41%) - LeetSync

--- 05_binary_search/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 05_binary_search/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,21 +1,16 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        # brute force solution
-        pos=[]
-        neg=[]
+        # optimal solution
+        result = [0]*len(nums)
+        p = 0
+        n = 1
+
         for i in nums:
             if i>=0:
-                pos.append(i)
+                result[p]=i
+                p+=2
             else:
-                neg.append(i)
-
-        # inplace change the variables
-        for i in range(0,len(pos)):
-            nums[2*i]=pos[i]
-            nums[2*i+1]=neg[i]
-        return nums
-
-
-        # time complexity is O(N+N/2)
-
-        # space complexity is O(N)
+                result[n]=i
+                n+=2
+        
+        return result
