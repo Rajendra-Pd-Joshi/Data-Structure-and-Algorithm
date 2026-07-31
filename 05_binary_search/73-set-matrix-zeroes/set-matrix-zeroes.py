f139300b9138c96e5da42fbbdc8ace6b5bb39002
Rajendra Prasad Joshi <rajendraprasadjoshi22@gmail.com>
2026-07-31 14:19:04 +0530

    Time: 7 ms (58.25%) | Memory: 20.7 MB (23.11%) - LeetSync

--- 05_binary_search/73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 05_binary_search/73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,33 +1,42 @@
 class Solution:
-    def fillInfinity(self,matrix,row,col):
-        r = len(matrix)
-        c = len(matrix[0])
-        for i in range(0,c):
-            if matrix[row][i] != 0:
-                matrix[row][i] = float('inf')
-        
-        for j in range(0,r):
-            if matrix[j][col] != 0:
-                matrix[j][col] = float('inf')
-    
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        # i 'm taking the first column and first row to keep track of zeros
+        # step 1: if there is a zero in the first column make col0=1 and in first row make row0=1
+        
         r = len(matrix)
         c = len(matrix[0])
+        col0=0
+        row0=0
+        for i in range(0,r):
+            if matrix[i][0]==0:
+                col0 =1
+        
+        for j in range(0,c):
+            if matrix[0][j]==0:
+                row0=1
+        
+        # step 2: iterate over the matrix and if you found zero set matrix[i][0] =0 and matrix[0][j] = 0
 
-        # at first traversal convert the into inf
         for i in range(0,r):
             for j in range(0,c):
                 if matrix[i][j] == 0:
-                    self.fillInfinity(matrix,i,j)
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+                
+        # step 3: traverse form (1,1) and if matrix[i][0]==0 or matrix[0][j]==0 then put is 0
+        for i in range(1,r):
+            for j in range(1,c):
+                if matrix[i][0]==0 or matrix[0][j]==0:
+                    matrix[i][j]=0
         
-        # convert the filled inf into zero
-        for i in range(0,r):
+        # step 4 : handel the first row and first column
+        if col0==1:
+            for i in range(0,r):
+                matrix[i][0]=0
+        
+        if row0==1:
             for j in range(0,c):
-                if matrix[i][j] == float('inf'):
-                    matrix[i][j] =0
-
-
-# time complexity is fill infity is O(N+M) and called inside so O(N*M*(N+M))+O(N*M) is the complexity and space is O(1)
+                matrix[0][j]=0
